8f07523883927df7596807ed1cff17154803bd10
hazem <you@example.com>
2025-09-12 20:13:43 +0300

    update fitness function

--- genetic_algorithm/ga.py
+++ genetic_algorithm/ga.py
@@ -1,7 +1,12 @@
+import math
+
+
 class GeneticAlgorithm:
+      
 
       # constractor 
       def __init__(self,dataframe,target,X,Y,N_generations = 4,N_populations = 8,model_type='calssification',mutation_rate = 0.1):
+
             self.__df = dataframe
             self.__target = target
             self.__X = X
@@ -15,8 +20,21 @@ class GeneticAlgorithm:
       def __init_population(self):
             pass
 
-      def __fitness(self):
-            pass
+      def fitness(self, chromosome):
+            columns = [self.__X.columns[i] for i, bit in enumerate(chromosome) if bit == 1]
+            if len(columns) == 0:
+                     return 0.0
+            correlations = 0.0
+     
+            for col in columns:
+               res = self.__df[self.__target].corr(self.__df[col] )
+               res = float(res)
+            if not math.isnan(res):
+              correlations += abs(res)
+            return round(correlations * (correlations / len(columns)), 8)
+        
+
+           
       
       def __crossover(self,first_chromosome,second_chromosome):
             pass
