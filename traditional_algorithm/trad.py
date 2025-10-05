a2ebd5ab9dbf10dd5b2b0c550bccfd7770ff5711
KASSEM HUSSEIN <kassem.learn@gmail.com>
2025-10-05 04:28:21 +0300

    Change Traditional Algorithm to F-test for features selection

--- traditional_algorithm/trad.py
+++ traditional_algorithm/trad.py
@@ -1,7 +1,9 @@
 from itertools import combinations
 from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import f_classif,f_regression
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.metrics import accuracy_score, r2_score
+import pandas as pd
 class TraditionalAlgorithm:
 
       def __init__(self,df,X,Y,target,model_type):
@@ -15,51 +17,42 @@ class TraditionalAlgorithm:
             '''
             self.__x= X
             self.__y= Y
-            self.__model= model_type
+            self.__model_select = f_classif if model_type == 'classification' else f_regression
+            self.__model= RandomForestClassifier() if model_type== 'classification' else RandomForestRegressor() 
+            self.__acc_f = accuracy_score  if model_type == 'classification' else r2_score
             self.__df = df
             self.__target = target
             self.__cache = {}
-      def __generate(self):
+      def __get_selected_features(self):
             '''
                   Args:
                         No thing
                         
-                  Return : List of  Possibale solution for select features ways
+                  Return : List of  selected feauture using SelectFromModel from scikit-learn 
             '''
-            all_combinations = []
-            for r in range(1, len(self.__x,) +1):
-                  all_combinations.extend(combinations(self.__x, r))
-            return all_combinations
-      
-      def __evaluation(self,combination):
-            '''
-                  Args:
-                        combination tuple of selected features
-                  return:
-                        sum absolute values of correleation for selected features with target column
+            X_train, X_test, Y_train, Y_test = train_test_split(
+                self.__x, self.__y, test_size=0.2, random_state=42
+            )      
+                  
+            f_scores, p_values = self.__model_select(X_train, Y_train)
 
-            '''
-            # avoid selected all feature
-            if len(combination) == self.__x.shape[1]:
-                  return 0.0
-            # avoid No features selected 
-            if len(combination) == 0:
-                  return 0.0
-            # accpet ratio of selected features should be lt 0.7
-            if ( len(combination) / self.__x.shape[1]) > 0.7:
-                  return 0.0
-            if combination in self.__cache:
-                  return self.__cache[combination]
-            corr = self.__df.corr()[self.__target].drop(self.__target).abs()
-            val  = corr[list(combination)].sum()
-            self.__cache[combination] =val
-            return val
-      def __predicate(self,combination):
+            # Combine into a DataFrame
+            feature_scores = pd.DataFrame({
+            'Feature': self.__x.columns,
+            'F Score': f_scores,
+            'P-value': p_values
+            }).sort_values(by='F Score', ascending=False)
+
+            # Select features with p < 0.05
+            selected = feature_scores[feature_scores['P-value'] < 0.05]
+            return list(selected['Feature'])
+      
+      def __predicate(self,features):
             '''
                   Args:
-                        combination tuple of selected features 
+                        list of features 
                   return 
-                        accuracy of combination  
+                        accuracy of model on selected features  
 
             '''
             
@@ -68,20 +61,15 @@ class TraditionalAlgorithm:
                 self.__x, self.__y, test_size=0.2, random_state=42
             )
             
-            # Choose model and evaluation metric based on problem type
-            if self.__model == 'classification':
-                model = RandomForestClassifier(random_state=42)
-                acc = accuracy_score
-            else:
-                model = RandomForestRegressor(random_state=42)
-                acc = r2_score
+            
+            
             
             # Train model and make predictions
             try:
 
-                model.fit(X_train[list(combination)], Y_train)
-                preds = model.predict(X_test[list(combination)])
-                accuracy = acc(Y_test, preds)
+                self.__model.fit(X_train[list(features)], Y_train)
+                preds = self.__model.predict(X_test[list(features)])
+                accuracy = self.__acc_f(Y_test, preds)
                 return accuracy
             except Exception as e:
                 # Handle any unexpected errors
@@ -95,16 +83,10 @@ class TraditionalAlgorithm:
                   Return:
                         tuple of accuracy and combination 
             '''
-            combinations = self.__generate()
-            items = []
-            for comb in combinations:
-                  val = self.__evaluation(combination=comb)
-                  items.append((val,comb))
-            items = sorted(items, key=lambda x: x[0], reverse=True)
-            item  = items[0] 
-            combination = item[1]
-            acc   = self.__predicate(combination=combination)
-            return (acc,combination)
+            selected_features =  self.__get_selected_features()
+            acc               =  self.__predicate(selected_features)
+            print((selected_features,acc))
+            return (acc,selected_features)
             
                   
             
